6c1951af1c01ec9c0a0e4961531a1febd12f3d36
cairnsi <59540148+cairnsi@users.noreply.github.com>
2020-02-25 22:17:19 -0600

    aa

--- ProjectHandlebars.js
+++ ProjectHandlebars.js
@@ -72,6 +72,50 @@ app.post('/', function(req,res,next){
 	res.render('home',context);
 });
 
+app.post('/findWeather', function(req,res,next){
+  if(checkSession(req,res)){
+	  return;
+  }
+  var context = setContext(req,res);
+  res.render('traveled',context);
+  if(req.body.cityName){
+	 request('http://api.openweathermap.org/data/2.5/weather?q='+req.body.cityName+'&APPID=' + credentials.owApiKey, function(err, response, body){
+		if(!err && response.statusCode < 400){
+		  var response = JSON.parse(body);
+		  var yourTemperature = Math.floor(response.main.temp - 273)+ " C";
+		  
+		   request('http://api.openweathermap.org/data/2.5/weather?q=seattle&APPID=' + credentials.owApiKey, function(err, response, body){
+			if(!err && response.statusCode < 400){
+			  var response = JSON.parse(body);
+			  var myTemperature = Math.floor(response.main.temp - 273)+ " C";
+			  
+			  var diffTemp = yourTemperature - myTemperature;
+			  context.weatherMessage = "It is "+ yourTemperature+ "in "+req.body.cityName;
+			  res.render('traveled',context);
+			  
+			} else {
+				next(err);
+			}
+			
+		  });
+		  
+		} else {
+		  if(response.statusCode >= 400 && response.statusCode < 500){
+			var context= {};
+			context.error = "City not found. Please try again";
+			res.render('getSession',context);
+			
+		  }else{
+			next(err);
+		  }
+		}
+		
+	  });
+  }else{
+	res.render('traveled',context);
+  }
+});
+
 app.get('/professional',function(req,res){
   if(checkSession(req,res)){
 	  return;
