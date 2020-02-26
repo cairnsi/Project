e2ce8647ff08aa3aa4b500c17cf2358917355ee3
cairnsi <59540148+cairnsi@users.noreply.github.com>
2020-02-25 22:40:07 -0600

    af

--- ProjectHandlebars.js
+++ ProjectHandlebars.js
@@ -81,17 +81,17 @@ app.post('/findWeather', function(req,res,next){
 	 request('http://api.openweathermap.org/data/2.5/weather?q='+req.body.cityName+'&APPID=' + credentials.owApiKey, function(err, response, body){
 		if(!err && response.statusCode < 400){
 		  var response = JSON.parse(body);
-		  var yourTemperature = Math.floor(response.main.temp - 273)+ " C";
+		  var yourTemperature = Math.floor(response.main.temp - 273);
 		  
 		   request('http://api.openweathermap.org/data/2.5/weather?q=seattle&APPID=' + credentials.owApiKey, function(err, response, body){
 			if(!err && response.statusCode < 400){
 		      var context = setContext(req,res);
 			  var response = JSON.parse(body);
-			  var myTemperature = Math.floor(response.main.temp - 273)+ " C";
+			  var myTemperature = Math.floor(response.main.temp - 273);
 			  
 			  var diffTemp = yourTemperature - myTemperature;
 			  var weatherMessage = "todays weather"
-			  context.weatherMessage = "It is "+ yourTemperature+ " in "+req.body.cityName+ ". ";
+			  context.weatherMessage = "It is "+ yourTemperature+ " C in "+req.body.cityName+ ". ";
 			  if(diffTemp>0){
 				  context.weatherMessage = context.weatherMessage + " That sounds nice! That is " + diffTemp + " warmer than Seattle";
 			  }else{
