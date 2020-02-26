4cc81d504d22d348e7e4d96168f131edc58c6935
cairnsi <59540148+cairnsi@users.noreply.github.com>
2020-02-25 23:18:38 -0600

    af

--- ProjectHandlebars.js
+++ ProjectHandlebars.js
@@ -115,7 +115,7 @@ app.post('/stock', function(req,res,next){
 		if(!err && response.statusCode < 400){
 		  var response = JSON.parse(body);
 		  var context = setContext(req,res);
-		  context.stockMessage = response[1];
+		  context.stockMessage = body;
 		  res.render('hobbies',context);
 		   
 		  
