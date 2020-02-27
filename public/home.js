08d23cf98a95473c959ffcde2a4cf1b423543e47
cairnsi <59540148+cairnsi@users.noreply.github.com>
2020-02-26 20:57:16 -0600

    afa

--- public/home.js
+++ public/home.js
@@ -1,1 +1,10 @@
-console.log("home.js is Here");
+function setCarousel(){
+	var i = 0;
+	var images = [];
+	var time = 2000;
+	images[0] = "images/ski1.jpeg";
+	images[1] = "inages/sail.jpeg";
+	images[2] = "images/ski2.png";
+	document.carousel.src = images[0];
+}
+setCarousel();
